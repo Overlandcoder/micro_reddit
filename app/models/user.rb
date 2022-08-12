4e04355c9a219db7a63ebc3686b07a803215849c
Overlander <anuragnandal@hotmail.com>
2022-08-11 21:47:13 -0400

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { in: 6..15 }, uniqueness: true
+  validates :password, presence: true, length: { in: 8..20 }
+  validates :email, presence: true, uniqueness: true
 end
