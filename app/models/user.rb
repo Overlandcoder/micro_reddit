929a5bed7da70486a903c1a26d9f01cbbfd9f3ca
Overlander <anuragnandal@hotmail.com>
2022-08-12 09:20:01 -0400

    Add associations between Post/User and Comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :username, presence: true, length: { in: 6..15 }, uniqueness: true
   validates :password, presence: true, length: { in: 8..20 }
