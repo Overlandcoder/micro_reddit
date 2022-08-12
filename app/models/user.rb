1480fd8da73811c5a5b86d22e8be0908456b6952
Overlander <anuragnandal@hotmail.com>
2022-08-12 07:41:20 -0400

    Add associations between User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, length: { in: 6..15 }, uniqueness: true
   validates :password, presence: true, length: { in: 8..20 }
   validates :email, presence: true, uniqueness: true
